000cd27c0d064cd6cfb0f8608e3b644c344e9e1e
AlexisSossa <alexis.dev.sosa@gmail.com>
2022-09-12 19:17:47 -0500

    finish

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import inquirer from "inquirer";
+import fs from "node:fs/promises";
+import { isBoxedPrimitive } from "node:util/types";
 
 const flags = [];
 process.argv.forEach((arg) => {
@@ -7,7 +9,7 @@ process.argv.forEach((arg) => {
   }
 });
 
-// console.log(flags);
+console.log(flags);
 
 if (flags.includes("a") || flags.includes("add")) {
   addQuestion();
@@ -15,51 +17,35 @@ if (flags.includes("a") || flags.includes("add")) {
   askQuestion();
 }
 
-// console.log(process.argv);
+console.log(process.argv);
 
 async function askQuestion() {
+  const data = await fs.readFile("./data.json");
+  const parsedData = JSON.parse(data.toString());
+
+  const target = parsedData[0];
+
+  const { question, answer } = target;
+
   const answers = await inquirer.prompt([
-    {
-      type: "list",
-      name: "position",
-      message: "What is your IT position?",
-      choices: [
-        "Software Enginner",
-        "UX/UI",
-        "DevOps",
-        "Product Manager",
-        "Security Enginner",
-        "Technical Recruiter",
-      ],
-    },
-    {
-      type: "input",
-      name: "live",
-      message: "Where do you live?",
-    },
-    {
-      type: "list",
-      name: "work",
-      message: "Where do you work at?",
-      choices: [
-        "Amazon",
-        "Vercel",
-        "Google",
-        "Microsoft",
-        "Auth0",
-        "Linkedin",
-        "Nowports",
-        "Netflix",
-        "Other",
-      ],
-    },
+    { type: "input", name: "useranswer", message: question },
   ]);
 
-  console.log(`Your position is ${answers.position}`);
-  console.log(`You live in ${answers.live}`);
-  console.log(`You work at ${answers.work}`);
+  target.lastAnsweredCorrect = checkAnswer(answers.useranswer, answer);
+  target.lastAsked = Date().toString();
 
-  console.log(
-    "Thanks for your aplication, if you fit the profile we will look for you!",
-  );
+  const newData = parsedData.filter((item) => item.id !== target.id);
+  newData.push(target);
+
+  await fs.writeFile("./data.json", JSON.stringify(newData));
+}
+
+function checkAnswer(input, answer) {
+  if (input === answer) {
+    console.log("That's right!");
+    return true;
+  } else {
+    console.log("Not this time!");
+    return false;
+  }
 }
